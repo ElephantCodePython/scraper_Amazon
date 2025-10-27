11b547623662b609ca01ea3c79b6231ec88ce9cd
elephant <elephant.code.small@gmail.com>
2025-10-27 13:38:53 +0330

    Update settings.py

--- product/settings.py
+++ product/settings.py
@@ -1,54 +1,56 @@
-# ---------- Bot information ----------
-BOT_NAME = "product"
-SPIDER_MODULES = ["product.spiders"]
-NEWSPIDER_MODULE = "product.spiders"
+# ______ BOT _______
+BOT_NAME = "product_amazon"
+SPIDER_MODULES = ["product_amazon.spiders"]
+NEWSPIDER_MODULE = "product_amazon.spiders"
 ADDONS = {}
-
-# ---------- Robots.txt ----------
+# Obey robots.txt rules
 ROBOTSTXT_OBEY = False
-
-# ---------- Concurrency and throttling ----------
-CONCURRENT_REQUESTS = 2
-CONCURRENT_REQUESTS_PER_DOMAIN = 2
-DOWNLOAD_DELAY = 4
-RANDOMIZE_DOWNLOAD_DELAY = True
-
-# ---------- Cookies ----------
+LOG_LEVEL = 'INFO'
+# Concurrency and throttling settings
+CONCURRENT_REQUESTS = 4
+CONCURRENT_REQUESTS_PER_DOMAIN = 4
+# Disable cookies (enabled by default)
 COOKIES_ENABLED = True
-
 # ---------- Playwright settings ----------
 DOWNLOAD_HANDLERS = {
     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 }
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
 PLAYWRIGHT_LAUNCH_OPTIONS = {
     "headless": False,
     "timeout": 60000
 }
-
+PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT = 90000
+# ___________ Proxy ___________
+#
+# ( input proxy and token )
+#
+# PLAYWRIGHT_CONTEXTS = {
+#     "proxy_rotating": {
+#         "proxy": {
+#             "server": "proxy",
+#             "username": "token",
+#         },
+#         "java_script_enabled": True,
+#         "ignore_https_errors": True,
+#     }
+# }
 # ---------- Spider middlewares ----------
-SPIDER_MIDDLEWARES = {
-   "product.middlewares.FakeHeaders": 300,
-   "product.middlewares.ProxyMiddleware": 200
+DOWNLOADER_MIDDLEWARES = {
+   "product_amazon.middlewares.FakeHeaders": 300,
 }
-
-# ---------- Item pipelines ----------
 ITEM_PIPELINES = {
-   "product.pipelines.ProductDatabasePipeline": 300,
+   'product_amazon.pipelines.ProductDatabasePipeline': 200,
 }
-
 # ---------- AutoThrottle ----------
 AUTOTHROTTLE_ENABLED = True
 AUTOTHROTTLE_START_DELAY = 1
 AUTOTHROTTLE_MAX_DELAY = 60
-
-
 # ---------- Timeouts / retries ----------
 DOWNLOAD_TIMEOUT = 60
 RETRY_ENABLED = True
 RETRY_TIMES = 5
 RETRY_HTTP_CODES = [429, 500, 502, 503, 504, 522, 524]
-
 # ---------- Feed encoding ----------
 FEED_EXPORT_ENCODING = "utf-8"
