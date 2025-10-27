80829f51e722696de095bdbbfb10045d8ffab881
elephant <elephant.code.small@gmail.com>
2025-10-27 13:42:14 +0330

    items.py

--- product/items.py
+++ product/items.py
@@ -1,12 +1,12 @@
 import scrapy
 
 class ProductItem(scrapy.Item):
-    category = scrapy.Field()
-    url = scrapy.Field()
-    price = scrapy.Field()
+    asin = scrapy.Field()
     title = scrapy.Field()
+    price = scrapy.Field()
     stars = scrapy.Field()
     rating = scrapy.Field()
     information = scrapy.Field()
-
-
+    img = scrapy.Field()
+    category = scrapy.Field()
+    url = scrapy.Field()
